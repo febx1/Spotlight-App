39a4b6eabdfa2df1e3c6c25d9269ddf2ed67a013
febx1 <60842703+febx1@users.noreply.github.com>
2025-03-25 16:55:15 +0530

    Auth and DB init(Convex)

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,26 +1,17 @@
 import { Stack } from "expo-router";
 import { SafeAreaView, StatusBar } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import { tokenCache } from "@clerk/clerk-expo/token-cache";
-import { ClerkProvider } from "@clerk/clerk-expo";
 import InitialLayout from "@/components/InitialLayout";
-
-const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
-
-if (!publishableKey) {
-  throw new Error(
-    "Missing Publishable Key. Please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env"
-  );
-}
+import ClerkAndConvexProvider from "@/providers/ClerkAndConvexProvider";
 
 export default function RootLayout() {
   return (
-    <ClerkProvider tokenCache={tokenCache} publishableKey={publishableKey}>
+    <ClerkAndConvexProvider>
       <SafeAreaProvider>
         <SafeAreaView style={{ flex: 1, backgroundColor: "#000" }}>
           <InitialLayout />
         </SafeAreaView>
       </SafeAreaProvider>
-    </ClerkProvider>
+    </ClerkAndConvexProvider>
   );
 }
